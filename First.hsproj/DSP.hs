1b71dcd071677660bed9caab4cb54c38d28aaa8c
Jay Kickliter <jay@kickliter.com>
2015-09-24 16:17:54 -0600

    Added FIR DSP routines

--- First.hsproj/DSP.hs
+++ First.hsproj/DSP.hs
@@ -2,6 +2,17 @@ module DSP where
 
 import qualified Data.Vector.Unboxed as U
 
+π = pi
+
+sinpi :: Floating a => a -> a
+sinpi x = sin $ π * x 
+
+sinc :: (RealFloat a) => a -> a
+sinc x = case x of
+  0 -> 1
+  _ -> sinpi x / (π*x)
+
+
 hamming :: Int -> Int -> Double
 hamming n len = 0.54 - 0.46 * cos((2 * pi * n') / (len' - 1))
   where
@@ -23,4 +34,13 @@ hamming'' len = U.generate len go
 dotp :: (U.Unbox a, Num a) => U.Vector a -> U.Vector a -> a
 dotp v w = U.sum $ U.zipWith (*) v w
 
+epsilon :: RealFloat a => a
+epsilon = encodeFloat 1 (fromIntegral $ 1-floatDigits epsilon)
 
+firPrototype :: Double -> Int -> Int -> Double
+firPrototype ω len n = ω * sinc(ω * (n' - (len' - 1) / 2))
+  where
+    n' = fromIntegral n
+    len' = fromIntegral len
+
+firPrototype' ω len = [firPrototype ω len n | n <- [0..len-1]]
