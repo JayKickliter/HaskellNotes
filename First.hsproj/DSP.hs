6079755436a7da7d2c4219d351f0e4815cac6bc2
Jay Kickliter <jay@kickliter.com>
2015-09-25 17:05:30 -0600

    Added more DSP code

--- First.hsproj/DSP.hs
+++ First.hsproj/DSP.hs
@@ -2,45 +2,52 @@ module DSP where
 
 import qualified Data.Vector.Unboxed as U
 
-π = pi
-
+-- | sinpi x == sin πx
 sinpi :: Floating a => a -> a
-sinpi x = sin $ π * x 
+sinpi x = sin $ pi * x
 
+-- | sinc x == (sin πx)/πx
 sinc :: (RealFloat a) => a -> a
 sinc x = case x of
   0 -> 1
-  _ -> sinpi x / (π*x)
+  _ -> sinpi x / (pi*x)
+
+-- | Computes dot product of two vectors
+dotp :: (U.Unbox a, Num a) => U.Vector a -> U.Vector a -> a
+dotp v w = U.sum $ U.zipWith (*) v w
 
 
-hamming :: Int -> Int -> Double
-hamming n len = 0.54 - 0.46 * cos((2 * pi * n') / (len' - 1))
+-- | Computes Hamming window values
+hamming' :: Int -> Int -> Double
+hamming' len n = 0.54 - 0.46 * cos((2 * pi * n') / (len' - 1))
   where
     n'   = fromIntegral n
     len' = fromIntegral len
 
-
-hamming' :: Int -> [Double]
-hamming' n = [0.54 - 0.46 * cos((2 * pi * n') / m) | n' <- [0..m]]
+-- | Creates a Hamming window vector
+hamming :: Int -> U.Vector Double
+hamming len = U.generate len go
   where
-    m =  fromIntegral (n - 1)
+    go = \x -> hamming' x len
 
-
-hamming'' :: Int -> U.Vector Double
-hamming'' len = U.generate len go
-  where
-    go = \x -> hamming x len
-
-dotp :: (U.Unbox a, Num a) => U.Vector a -> U.Vector a -> a
-dotp v w = U.sum $ U.zipWith (*) v w
-
-epsilon :: RealFloat a => a
-epsilon = encodeFloat 1 (fromIntegral $ 1-floatDigits epsilon)
-
-firPrototype :: Double -> Int -> Int -> Double
-firPrototype ω len n = ω * sinc(ω * (n' - (len' - 1) / 2))
+-- | Computes FIR filter prototype values
+prototype' :: (RealFloat a) => a -> Int -> Int -> a
+prototype' ω len n = ω * sinc(ω * (n' - (len' - 1) / 2))
   where
     n' = fromIntegral n
     len' = fromIntegral len
 
-firPrototype' ω len = [firPrototype ω len n | n <- [0..len-1]]
+-- | Creates a prototype FIR filter vector
+prototype :: (U.Unbox a, RealFloat a) => a -> Int -> U.Vector a
+prototype ω len = U.generate len go
+  where
+    go = \x -> prototype' ω len x
+
+
+-- | Create windowed taps
+--taps :: (Int a, RealFloat b) => b -> (b -> a -> a -> b) -> U.Vector b
+taps ω len winFun = U.generate len go where
+  go = (prototype' ω len) . (winFun len)
+
+add :: (Num a, U.Unbox a) => U.Vector a -> U.Vector a -> U.Vector a
+add = U.zipWith (Prelude.+)
